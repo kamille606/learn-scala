51ddf5cf63e43e81f874dcf31c836e49779f6c63
Administrator <kamille606@163.com>
2022-05-08 17:31:09 +0800

    day01

--- src/main/scala/day02/Test05FileIo.scala
+++ src/main/scala/day02/Test05FileIo.scala
@@ -5,8 +5,9 @@ import scala.io.Source
 
 object Test05FileIo {
   def main(args: Array[String]): Unit = {
-    Source.fromFile("src/main/resources/test.txt").foreach(print)
-
+    val source = Source.fromFile("src/main/resources/test.txt")
+    source.foreach(print)
+    source.close()
     val writer =  new PrintWriter(new File("src/main/resources/output.txt"))
     writer.write("hello scala from java writer")
     writer.close()

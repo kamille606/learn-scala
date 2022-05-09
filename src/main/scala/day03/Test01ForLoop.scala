880ef55c692338b34b14bf7d65869ea28a119b23
Administrator <kamille606@163.com>
2022-05-10 00:30:05 +0800

    day03

--- src/main/scala/day03/Test01ForLoop.scala
+++ src/main/scala/day03/Test01ForLoop.scala
@@ -1,5 +1,7 @@
 package day03
 
+import scala.language.postfixOps
+
 object Test01ForLoop {
   def main(args: Array[String]): Unit = {
 
@@ -13,6 +15,35 @@ object Test01ForLoop {
       println(i)
     }
     for (i <- 1 until 10) {
+      println(i)
+    }
+
+    for (i <- Array(12, 23 ,34)) {
+      println(i)
+    }
+    for (i <- Set(12, 23 ,34)) {
+      println(i)
+    }
+
+    for (i <- 1 to 5) {
+      if (i != 5) {
+        println(i)
+      }
+    }
+    for (i <- 1 to 10 if i != 5) {
+      println(i)
+    }
+
+    for (i <- 1 to 10 by 2) {
+      println(i)
+    }
+    for (i <- 1 .to(10, 2)) {
+      println(i)
+    }
+    for (i <- 1 to 10 reverse) {
+      println(i)
+    }
+    for (i <- 10 to 1 by -1) {
       println(i)
     }
 

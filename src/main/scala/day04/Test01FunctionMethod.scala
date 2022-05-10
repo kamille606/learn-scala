4ef0c2e58bab149b9f30f2aead655d072f741ed6
Administrator <kamille606@163.com>
2022-05-10 18:36:47 +0800

    day04

--- src/main/scala/day04/Test01FunctionMethod.scala
+++ src/main/scala/day04/Test01FunctionMethod.scala
@@ -2,13 +2,15 @@ package day04
 
 object Test01FunctionMethod {
   def main(args: Array[String]): Unit = {
-    //定义函数
+    //定义函数：返回值为Unit
     def sayHi(name: String): Unit = {
       println(s"hi,${name}")
     }
+    //定义函数：返回值为箭头函数
     def sayGo(name: String) = (age: String) => {
       println(s"go,${name},${age}")
     }
+    //赋值箭头函数：返回值为Unit
     val sayYes = (name: String) => {
       println(s"yes,${name}")
     }
@@ -18,7 +20,7 @@ object Test01FunctionMethod {
     sayYes("alice")
     Test01FunctionMethod.sayHi("bob")
   }
-  //定义方法
+  //定义方法：返回值为Unit
   def sayHi(name: String): Unit = {
     println(s"Hi,${name}")
   }

6a2a34e5ec90fca70a8ce51c892601336406b8df
Administrator <kamille606@163.com>
2022-05-13 00:18:46 +0800

    day05

--- src/main/scala/day04/Test05Lambda.scala
+++ src/main/scala/day04/Test05Lambda.scala
@@ -9,11 +9,11 @@ object Test05Lambda {
       println(name)
     }
 
-    def f(func: String => Unit): Unit = {
-      func("alice")
+    def f(func: String => Unit, name: String): Unit = {
+      func(name)
     }
-    f(fun1)
-    f(fun2)
-    f((name: String) => {println(name)})
+    f(fun1, "函数=匿名函数(lambda)")
+    f(fun2, "函数")
+    f((name: String) => {println(name)}, "匿名函数(lambda)")
   }
 }

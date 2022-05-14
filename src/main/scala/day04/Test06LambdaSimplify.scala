fc52690744eb3aba23cd56f68a80ff8450dcc53a
Administrator <kamille606@163.com>
2022-05-14 23:30:37 +0800

    day06

---
+++ src/main/scala/day04/Test06LambdaSimplify.scala
@@ -0,0 +1,31 @@
+package day04
+
+object Test06LambdaSimplify {
+  def main(args: Array[String]): Unit = {
+    def f(func: String => Unit): Unit = {
+      func("alice")
+    }
+
+    f((name) => {
+      println(name)
+    })
+    f(name => {
+      println(name)
+    })
+    f(x => {
+      println(x)
+    })
+    f(x => println(x))
+    f(println(_))
+    f(println)
+
+    def dualFunctionOneAndTwo(fun: (Int, Int) => Int): Int = {
+      fun(1, 2)
+    }
+
+    val add = (a: Int, b: Int) => a + b
+    val minus = (a: Int, b: Int) => a - b
+    println(dualFunctionOneAndTwo(add))
+    println(dualFunctionOneAndTwo(minus))
+  }
+}
